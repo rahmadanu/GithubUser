245dff0157d04f29337356f20e8c200c7fdc4e0c
rahmadanu <96525733+rahmadanu@users.noreply.github.com>
2022-03-09 00:50:00 +0700

    Testing Commit

--- app/src/main/java/com/android/githubuser/MainActivity.kt
+++ app/src/main/java/com/android/githubuser/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val list = ArrayList<User>()
 
+    //this is testing
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
